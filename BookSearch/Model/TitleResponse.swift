efa5fb511ff42084ab8597194eee0d3bb8c98bb0
youjong <pyj078@naver.com>
2022-11-10 18:16:51 +0900

    Git test

---
+++ BookSearch/Model/TitleResponse.swift
@@ -0,0 +1,21 @@
+//
+//  TitleResponse.swift
+//  BookSearch
+//
+//  Created by You Jong Park on 2022/11/10.
+//
+
+// MARK: - 책 목록에는 책의 커버 이미지, 제목, 저자, 출판일 , 검색된 책의 총 수량 을 볼 수 있다.
+
+//https://www.googleapis.com/books/v1/volumes?q=having&fields=totalItems,items(volumeInfo/title,volumeInfo/authors,volumeInfo/publishedDate,volumeInfo/imageLinks)
+
+import Foundation
+
+
+
+        
+        
+        
+        
+
+
